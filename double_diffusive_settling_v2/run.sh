4d21c5e1e74e81df4eabc5e865146fafd73e2155
cliu124 <39631690+cliu124@users.noreply.github.com>
2023-03-08 11:16:20 -0800

    Update

--- double_diffusive_settling_v2/run.sh
+++ double_diffusive_settling_v2/run.sh
@@ -4,5 +4,4 @@ mpiexec -n 4 python3 -m dedalus merge_procs snapshots
 mpiexec -n 4 python3 plot_slices.py snapshots/*.h5
 name="frames_$(date +"%Y%m%d%H%M%S")"
 mv frames "$name"
-mv snapshots "snapshots_$(date +"%Y%m%d%H%M%S")"
 cp -r "$name" /mnt/d/Data/dedalus_example/double_diffusive_settling_v2
