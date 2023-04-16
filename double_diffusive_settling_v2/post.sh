925dca7e9a7c260bd56f35b519a2088be90b13ff
cliu124 <39631690+cliu124@users.noreply.github.com>
2023-04-15 18:57:55 -0700

    Update post.sh for settling

--- double_diffusive_settling_v2/post.sh
+++ double_diffusive_settling_v2/post.sh
@@ -1,12 +1,12 @@
 conda activate dedalus2
-mpiexec -n 4 python3 -m dedalus merge_procs snapshots
-mpiexec -n 4 python3 plot_slices.py snapshots/*.h5
+mpiexec -n 4 python3 -m dedalus merge_procs analysis
+mpiexec -n 4 python3 plot_slices.py analysis/*.h5
 today="$(date +"%Y%m%d%H%M%S")"
 frames_name="frames_$today"
 mv frames "$frames_name"
 cp -r "$frames_name" /mnt/d/Data/dedalus_example/double_diffusive_settling_v2
-cp double_diffusive_settling.py snapshots/
-cp plot_slices.py snapshots/
-snapshots_name="snapshots_$today"
-mv snapshots "$snapshots_name"
-cp -r "$snapshots_name" /mnt/d/Data/dedalus_example/double_diffusive_settling_v2
+cp double_diffusive_settling.py analysis/
+cp plot_slices.py analysis/
+analysis_name="analysis_$today"
+mv analysis "$analysis_name"
+cp -r "$analysis_name" /mnt/d/Data/dedalus_example/double_diffusive_settling_v2
