c2e61fa1d3d3141b91eaa19ca6e0287e6af69004
cliu124 <39631690+cliu124@users.noreply.github.com>
2023-06-30 06:23:38 -0700

    Update

--- inclined_porous_convection_v2/inclined_porous_convection.py
+++ inclined_porous_convection_v2/inclined_porous_convection.py
@@ -176,37 +176,37 @@ elif flag.collision1==0 and flag.collision2!=0:
         tmp1['c']=solver.state['T']['c']
         tmp1.require_layout(domain.dist.layouts[1])
         tmp1.data[1:,:]=tmp1.data[:0:-1,::-1]
-        solver.state['T']['g'][0:flag.Nx-1,0:flag.Nz-1]=-tmp1['g']
+        solver.state['T']['g'](scales=1)=-tmp1['g']
         
         tmp2=domain.new_field()
         tmp2['c']=solver.state['Tz']['c']
         tmp2.require_layout(domain.dist.layouts[1])
         tmp2.data[1:,:]=tmp1.data[:0:-1,::-1]
-        solver.state['Tz']['g'][0:flag.Nx-1,0:flag.Nz-1]=tmp2['g']
+        solver.state['Tz']['g'](scales=1)=tmp2['g']
         
         tmp3=domain.new_field()
         tmp3['c']=solver.state['w']['c']
         tmp3.require_layout(domain.dist.layouts[1])
         tmp3.data[1:,:]=tmp1.data[:0:-1,::-1]
-        solver.state['w']['g'][0:flag.Nx-1,0:flag.Nz-1]=-tmp3['g']
+        solver.state['w']['g'](scales=1)=-tmp3['g']
         
         tmp4=domain.new_field()
         tmp4['c']=solver.state['wz']['c']
         tmp4.require_layout(domain.dist.layouts[1])
         tmp4.data[1:,:]=tmp1.data[:0:-1,::-1]
-        solver.state['wz']['g'][0:flag.Nx-1,0:flag.Nz-1]=tmp4['g']
+        solver.state['wz']['g'](scales=1)=tmp4['g']
         
         tmp5=domain.new_field()
         tmp5['c']=solver.state['u']['c']
         tmp5.require_layout(domain.dist.layouts[1])
         tmp5.data[1:,:]=tmp1.data[:0:-1,::-1]
-        solver.state['u']['g'][0:flag.Nx-1,0:flag.Nz-1]=-tmp5['g']
+        solver.state['u']['g'](scales=1)=-tmp5['g']
         
         tmp6=domain.new_field()
         tmp6['c']=solver.state['p']['c']
         tmp6.require_layout(domain.dist.layouts[1])
         tmp6.data[1:,:]=tmp1.data[:0:-1,::-1]
-        solver.state['p']['g'][0:flag.Nx-1,0:flag.Nz-1]=tmp6['g']
+        solver.state['p']['g'](scales=1)=tmp6['g']
     if flag.restart_t0:
         solver.sim_time=0
         fh_mode='overwrite'
