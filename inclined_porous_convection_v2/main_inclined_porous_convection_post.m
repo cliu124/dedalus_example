ef2d61af7bab07ede62e3a6ecaeee86375b5874c
cliu124 <39631690+cliu124@users.noreply.github.com>
2023-06-30 22:16:45 -0700

    Update 2023/06/30

--- inclined_porous_convection_v2/main_inclined_porous_convection_post.m
+++ inclined_porous_convection_v2/main_inclined_porous_convection_post.m
@@ -3,7 +3,7 @@ close all;
 clc;
 
 %slurm_num={'20230408221941'};
-slurm_num={'20230617004227'}; %change this number as the output in your file.
+slurm_num={'20230630220531'}; %change this number as the output in your file.
 flag.print=1;
 flag.visible=1;
 flag.video=1;
@@ -17,8 +17,8 @@ for slurm_ind=1:length(slurm_num)
             slurm_num{slurm_ind},'\analysis\analysis_s1.h5'];
     end
     dedalus_post_my{slurm_ind}=post_inclined_porous_convection(h5_name,flag);
-%     dedalus_post_my{slurm_ind}=dedalus_post_my{slurm_ind}.snapshot('u',[],[],[100]);
-%     dedalus_post_my{slurm_ind}=dedalus_post_my{slurm_ind}.snapshot('w',[],[],[100]);
+    %dedalus_post_my{slurm_ind}=dedalus_post_my{slurm_ind}.snapshot('u',[],[],[100]);
+    dedalus_post_my{slurm_ind}=dedalus_post_my{slurm_ind}.snapshot('w',[],[],[100]);
     dedalus_post_my{slurm_ind}=dedalus_post_my{slurm_ind}.snapshot('T_tot',[],[],[]);
 %     dedalus_post_my{slurm_ind}=dedalus_post_my{slurm_ind}.z_slice('T_tot',[0.3,0.5,0.7],[10,50]);
     %dedalus_post_my{slurm_ind}=dedalus_post_my{slurm_ind}.spectrum_average('T');
