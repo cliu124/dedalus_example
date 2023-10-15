de451ca9c5f555e72ef0c9464310f6b89484a2fa
cliu124 <39631690+cliu124@users.noreply.github.com>
2023-10-14 19:49:43 -0700

    Update the IC for larger domain

--- inclined_porous_convection_v2/Extend_grid/extend_IC.m
+++ inclined_porous_convection_v2/Extend_grid/extend_IC.m
@@ -3,7 +3,8 @@ close all;
 
 read_folder='Lx_10_Nx_256';
 write_folder_list={'Lx_15_Nx_384','Lx_20_Nx_512',...
-    'Lx_30_Nx_768','Lx_40_Nx_1024'};
+    'Lx_30_Nx_768','Lx_40_Nx_1024',...
+    'Lx_80_Nx_2048','Lx_160_Nx_4096'};
 Nz=64;
 for write_folder_ind=1:length(write_folder_list)
     write_folder=write_folder_list{write_folder_ind};
@@ -18,6 +19,10 @@ for write_folder_ind=1:length(write_folder_list)
             Nx_write=768;
         case 'Lx_40_Nx_1024'
             Nx_write=1024;
+        case 'Lx_80_Nx_2048'
+            Nx_write=2048;
+        case 'Lx_160_Nx_4096'
+            Nx_write=4096;
     end
     for ind=1:6
        T=h5read_complex([read_folder,'/X',num2str(ind),'_checkpoint_s1.h5'],'/tasks/T'); 
