fa4e34fc559834ef315bea23932153b649e42d48
Chang Liu @ UConn ME <chang_liu@uconn.edu>
2025-09-05 21:35:42 -0400

    Update run.sh

--- magnetoconvection_single_mode/run.sh
+++ magnetoconvection_single_mode/run.sh
@@ -3,6 +3,6 @@ now="dedalus_$(date +"%Y%m%d%H%M%S")"
 mkdir -p "$now"
 cp -r *.py "$now"
 cd "$now"
-python3 magnetoconvection_v2.py > "$now" 2>&1
+python3 magnetoconvection_v2.py 2>&1 | tee dedalus_output_$(date +%Y%m%d_%H%M%S).out
 echo $now
 echo "finished"
