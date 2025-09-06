ed903e6d7ca2bd47b05a5b5ea9342f067877ad05
Chang Liu @ UConn ME <chang_liu@uconn.edu>
2025-09-06 10:34:50 -0400

    Update a steady state solution

--- magnetoconvection_single_mode/run.sh
+++ magnetoconvection_single_mode/run.sh
@@ -2,6 +2,7 @@ conda activate dedalus2
 now="dedalus_$(date +"%Y%m%d_%H%M%S")"
 mkdir -p "$now"
 cp -r *.py "$now"
+cp -r *.h5 "$now"
 cd "$now"
 python3 magnetoconvection_v2.py 2>&1 | tee dedalus_output_$(date +%Y%m%d_%H%M%S).out
 echo $now
